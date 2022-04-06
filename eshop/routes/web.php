c2d1e7c92273c8aef2ba4b4484b5be8043b0d14d
eckysaroyd <enyato98@gmail.com>
2022-04-06 07:46:41 +0530

    done with admin authentication

--- eshop/routes/web.php
+++ eshop/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,14 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => ['auth','isAdmin']], function () {
+
+
+
+ });
+ Route::middleware(['auth','isAdmin'])->group(function (){
+    Route::get('/dashboard', function () {
+        return "this is admin";
+     });
+ });
