1e1ada2c2b8f6262f387ba59e1c8ad8b67e1e11c
eckysaroyd <enyato98@gmail.com>
2022-09-29 23:14:54 +0530

    modification in admin layout and header

--- multi vendor ecommerce/multivendor_ecommerce/resources/views/admin/layout/layout.blade.php
+++ multi vendor ecommerce/multivendor_ecommerce/resources/views/admin/layout/layout.blade.php
@@ -19,7 +19,7 @@
   <!-- inject:css -->
   <link rel="stylesheet" href="{{ url('admin/css/vertical-layout-light/style.css') }}">
   <!-- endinject -->
-  <link rel="shortcut icon" href="{{ url('admin/images/favicon.png') }}" />
+  <link rel="shortcut icon" href="{{ asset('admin/images/favicon.png') }}" />
 </head>
 <body>
   <div class="container-scroller">
